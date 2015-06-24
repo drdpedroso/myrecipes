0c54640e9698e78d52783e45a8765ee34c7e69b8
drdpedroso <drdpedroso@gmail.com>
2015-06-24 16:49:43 +0000

    create chefs and recipes association and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604213113) do
+ActiveRecord::Schema.define(version: 20150624155324) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -20,11 +20,7 @@ ActiveRecord::Schema.define(version: 20150604213113) do
     t.datetime "updated_at"
   end
 
-  create_table "recipes", force: true do |t|
-    t.text     "summary"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "recipes" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
