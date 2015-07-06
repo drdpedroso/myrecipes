1ec88b991025e0a6809813f932d5d2022ef0de36
drdpedroso <drdpedroso@gmail.com>
2015-07-06 17:55:26 +0000

    Create new and edit form and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
  root 'pages#home'  #manda direto pra pagina home
- get "/home", to: 'pages#home'
+ get '/home', to: 'pages#home'
+# get "/home", to: 'pages#home' # http get requestion
+# get "/recipes", to:'recipes#index' #vai pro recipes controller e procura pelo index se for pro /routes
+# get "/recipes/new",to:'recipes#new',as: 'new_recipes' #será mostrado como new_recipe
+ #post "/recipes",to: 'recipes#create'
+# get "/recipes/:id/edit",to: 'recipes#edit',as: 'edit_recipe'
+# patch "/recipes/:id",to: "recipes#update"
+# get "/recipes/:id",to: "recipes#show",as: 'recipe'
+# delete "/recipes/:id",to: "recipes#destroy"
+ 
+ resources :recipes
 end
