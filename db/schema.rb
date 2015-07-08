a804cd83506f73cce57f5a3b9149b8ae2c262e3d
drdpedroso <drdpedroso@gmail.com>
2015-07-08 00:54:18 +0000

    Add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624155324) do
+ActiveRecord::Schema.define(version: 20150707195139) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
