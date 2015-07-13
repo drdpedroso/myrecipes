74e255a426b2d9283c26e22e73f06c5249b3a5a1
drdpedroso <drdpedroso@gmail.com>
2015-07-13 22:58:53 +0000

    Secure Commit

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,23 @@
+class ChefsController < ApplicationController
+  
+  def new
+    @chef = Chef.new
+  end
+  
+  def create
+    binding.pry
+    
+    
+  end
+  
+  def edit
+    
+    
+  end
+  
+  def update
+    
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
  
  resources :recipes do
   member do
-   
    post 'like'
-   
   end
  end
+ 
+ resources :chefs, except: [:new]
+ 
+ get '/register', to: "chefs#new"
 end
