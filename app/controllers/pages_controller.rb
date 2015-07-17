b8e7d6a64e5d98af1b9584d685f233f845643fd6
drdpedroso <drdpedroso@gmail.com>
2015-07-17 21:16:19 +0000

    Create cehfs resources and add authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   
   def home
+    redirect_to recipes_path if logged_in? #se estiver logado envia direto pra pagina de receitas
+    
   end
   
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,10 @@
 class RecipesController < ApplicationController
   
+  before_action :set_recipe, only: [:edit,:update,:show,:like]
+  before_action :require_user, except: [:show,:index]
+  before_action :require_same_user, only: [:edit,:update]
+  
+  
   def index
     #@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
     @recipes = Recipe.paginate(page: params[:page], per_page:4)
@@ -15,7 +20,8 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
+    @recipe.chef = current_user
+    
     
     if @recipe.save
       flash[:success] = "Your recipe was created!"
@@ -26,11 +32,10 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+    
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:success] = "Your recipe was updated succeesfully"
       redirect_to recipe_path(@recipe)
@@ -42,8 +47,8 @@ class RecipesController < ApplicationController
 
   
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was succeesfully made"
       redirect_to :back
@@ -59,5 +64,15 @@ class RecipesController < ApplicationController
       params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
     
- # end
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @recipe.chef
+        flash[:danger] = "You cna only edit your own recipes"
+        redirect_to recipes_path
+      end
+    end
+
 end
